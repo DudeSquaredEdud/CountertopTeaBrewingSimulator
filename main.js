f28d98580ce761bfe0e6990bf9a677279f0bba04
AshtonAndrepont <C00462480@louisiana.edu>
2025-02-01 21:39:55 -0600

    refined countertop

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 // Set up the scene, camera, and renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.2, 1000);
 const renderer = new THREE.WebGLRenderer();
+var mouse = new THREE.Vector2();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -23,10 +24,10 @@ function quickLoad(path) {
     });
 };
 
-quickLoad('meshes/countertop.gltf');
+quickLoad('meshes/countertop.glb');
 
 // Light!
-const ambientLight = new THREE.AmbientLight(0xafafaa, 5);
+const ambientLight = new THREE.AmbientLight(0xafafaa, 1);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xfaaa50, 1);
@@ -35,12 +36,30 @@ scene.add(directionalLight);
 
 
 // Position the camera
-camera.translateZ(3);
-camera.translateY(5);
-camera.rotateX(-.8);
+camera.translateZ(5);
+camera.translateY(6);
+camera.translateX(0);
+camera.rotateX(-.3);
 
 scene.translateY(0);
 
+// Move the camera with the mouse a little bit
+document.addEventListener('mousemove', onDocumentMouseMove, false);
+function onDocumentMouseMove(event) {
+    event.preventDefault();
+    mouse.x = (-(event.clientX / window.innerWidth)+.5)/5;
+    mouse.y = -.2+(-(event.clientY / window.innerHeight))/10;
+}
+
+// Update the camera based on the mouse position
+function updateCamera() {
+    // Adjust the camera's position based on the mouse's position
+    camera.rotation.y =  mouse.x;
+    camera.rotation.x =  mouse.y;
+    
+}
+
 renderer.setAnimationLoop(() => {
 renderer.render(scene, camera);
+updateCamera();
 });
